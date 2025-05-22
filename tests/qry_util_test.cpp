7f0a3d656488535eec29b5dbd734823d262d0b37
James Wang <james_wangx@163.com>
2025-05-22 14:35:28 +0800

    Update qry_util_test

--- tests/qry_util_test.cpp
+++ tests/qry_util_test.cpp
@@ -2,24 +2,27 @@
 
 #include <tcdefs.h>
 #include <tc/tc_startup.h>
+#include <tccore/aom_prop.h>
 
 #include "qry_util.hpp"
+#include "logger.hpp"
 
+extern logical debug;
 extern char *user;
 
-TEST(qry_util_test, QRY_UTIL_query_one_by_name) {
+TEST(qry_util_test, QRY_UTIL_query_one) {
     int rcode = ITK_ok;
     tag_t result_tag = NULLTAG;
-    char *result_uid = nullptr;
+    char *result_string = nullptr;
     std::map<std::string, std::string> params = {
         {"Type", "User_Inbox"},
         {"Owning User", user}
     };
 
     rcode = QRY_UTIL_query_one("General...", params, &result_tag);
-    EXPECT_EQ(rcode, ITK_ok) << "Failed to query one by name";
+    EXPECT_EQ(rcode, ITK_ok) << "Failed to query one";
     EXPECT_NE(result_tag, NULLTAG) << "Result tag is null";
 
-    ITK__convert_tag_to_uid(result_tag, &result_uid);
-    EXPECT_EQ(std::string("AwthCpYHJ72RwB"), result_uid);
+    LOGGER_ITK_PASS(AOM_ask_value_string(result_tag, "object_string", &result_string));
+    EXPECT_EQ(std::string("My Worklist"), std::string(result_string));
 }
